b3e693d8b2b7b03bf5654fa823893944e6ecc91f
SuperfastSimon <snellesimonissnel@gmail.com>
2026-02-17 16:39:22 +0100

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Enable if deploying to Vercel with Edge Functions
+  // experimental: { serverActions: true },
 }
 
 module.exports = nextConfig
